93a5a11edb144ae1c74ee1251990c4d86e9cbbbc
Themba G <goodnessthembac@gmail.com>
2023-02-17 17:53:33 +0200

    add files

---
+++ app.js
@@ -0,0 +1,28 @@
+const { json } = require("express")
+
+const exprees = require('express')
+const app = exprees()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json({ message: "view users at <http://localhost:3000/users> URL" })
+})
+
+app.get('/users', function (req, res) {
+    const users = [
+        { id: 1, name: 'Jakaza' },
+        { id: 2, name: 'Themba' },
+        { id: 4, name: 'Ntshepo' },
+        { id: 5, name: 'Ntshovelo' },
+        { id: 6, name: 'Khensani' },
+        { id: 7, name: 'Heery' },
+        { id: 8, name: 'Vekani' },
+    ]
+
+    res.json(users)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
